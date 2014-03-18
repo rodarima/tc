ec6bfd6c98c7588073462b4eb4db6ff0ca63f45c
Rodrigo Arias <rodarima@gmail.com>
2014-03-18 23:06:57 +0100

    Algoritmo de reduccion de no-generadores, en fase de pruebas

--- p1/src/grammar_reduce.c
+++ p1/src/grammar_reduce.c
@@ -1,32 +1,17 @@
 #include "grammar_reduce.h"
 #include "grammar.h"
+#include "queue.h"
+#include "list.h"
+#include "dbg.h"
 
 
-int grammar_reduce_queue_check(struct queue_t queue, void *ptr)
+int is_connector_complete(struct connector_t *connector)
 {
-	struct connector_t *connector;
-
-	if(NODE_IS_TYPE(ptr, NODE_CON))
+	if(NODE_IS_MARKED(connector->sym, MARK_GEN))
 	{
-		connector = (struct connector_t *) ptr;
-		if(NODE_IS_MARKED(connector->sym, MARK_PASS))
+		if( (!connector->con) || NODE_IS_MARKED(connector->con, MARK_GEN) )
 		{
-			if(!connector->con)
-			{
-				return 1;
-			}
-			else if(NODE_IS_MARKED(connector->con))
-			{
-				return 1;
-			}
-			else
-			{
-				return 0;
-			}
-		}
-		else
-		{
-			return 0;
+			return 1;
 		}
 	}
 	return 0;
@@ -37,47 +22,136 @@ int grammar_reduce_no_generators(struct grammar_t *g)
 {
 	struct queue_t gen;
 	struct list_node_t *list_node;
-	void *tmp, *last;
-	struct symbol_t *sym;
+	void *last, *element, *from;
+	struct symbol_t *symbol;
+	struct connector_t *connector;
 
 	queue_empty(&gen);
 	
 	/* Get all terminals */
-	list_node = g->terminals.start
+	list_node = g->terminals.start;
 	while(list_node)
 	{
-		NODE_MARK(list_node->ptr, MARK_PASS);
-		return_if(queue_push(&gen, list_node->ptr), -1);
+		debug("Adding %p\n", list_node->ptr);
+		NODE_MARK(list_node->ptr, MARK_GEN);
+		if(queue_push(&gen, list_node->ptr))
+		{
+			queue_clear(&gen);
+			return -1;
+		}
+		last = list_node->ptr;
 		list_node = list_node->next;
 	}
 
-	return_if(!gen.start, -1);
+	return_if(!gen.start, 0);
 
 	/* For each terminal, find all generators */
 	do
 	{
-		
-		tmp = gen.start->ptr;
-		if(NODE_IS_TYPE(tmp, NODE_CON))
+		if(queue_pop(&gen, &element))
 		{
-			//conn stuff
+			debug("ERROR FATAL");
+		}
+
+		if(NODE_IS_TYPE(element, NODE_CON))
+		{
+			if(NODE_IS_MARKED(element, MARK_GEN))
+			{
+				debug("This cannot happen %p", element);
+			}
+			if(!NODE_IS_MARKED(element, MARK_QUEUED))
+			{
+				debug("This cannot happen %p", element);
+			}
+			connector = (struct connector_t *) element;
+			if(is_connector_complete(connector))
+			{
+				NODE_MARK(element, MARK_GEN);
+				if(!IS_NODE_MARKED(connector->from, MARK_GEN))
+				{
+					if(!IS_NODE_TYPE(connector->from, NODE_CON))
+					{
+						NODE_MARK(connector->from, MARK_GEN);
+					}
+					if(!IS_NODE_MARKED(connector->from, MARK_QUEUED))
+					{
+						NODE_MARK(connector->from, MARK_QUEUED);
+						if(queue_push(&gen, connector->from))
+						{
+							queue_clear(&gen);
+							return -1;
+						}
+						last = connector->from;
+					}
+				}
+			}
+			else if(last == element)
+			{
+				break;
+			}
+			else
+			{
+				if(queue_push(&gen, element))
+				{
+					queue_clear(&gen);
+					return -1;
+				}
+			}
 		}
 		else
 		{
-			sym = (struct symbol_t *) tmp;
-			tmp = sym->from.start;
+			symbol = (struct symbol_t *) element;
+
+			list_node = symbol->from.start;
 			/* Find connections to the symbol */
-			while(tmp)
+			while(list_node)
 			{
-				if(!NODE_IS_MARKED(tmp->ptr, MARK_PASS))
+				from = list_node->ptr;
+				if(NODE_IS_MARKED(from, MARK_GEN))
 				{
-					return_if(queue_push(&queue, tmp->ptr), -1);
-					NODE_MARK(tmp->ptr, MARK_PASS);
+					debug("This cannot happen %p", from);
 				}
-				tmp = tmp->next;
+				if(!NODE_IS_MARKED(from, MARK_QUEUED))
+				{
+					if(queue_push(&gen, from))
+					{
+						queue_clear(&gen);
+						return -1;
+					}
+					NODE_MARK(from, MARK_QUEUED);
+				}
+				last = from;
+				list_node = list_node->next;
 			}
 		}
 	}
-	while(tmp != gen.start->ptr)
+	while(gen.start);
+
+	queue_clear(&gen);
+	
+	printf("Marked variables are:\n");
+	list_node = g->variables.start;
+	while(list_node)
+	{
+		symbol = (struct symbol_t *) list_node->ptr;
+		if(NODE_IS_MARKED(symbol, MARK_GEN))
+		{
+			grammar_symbol_print(symbol);
+		}
+		list_node = list_node->next;
+	}
+	printf("Unmarked variables are:\n");
+	list_node = g->variables.start;
+	while(list_node)
+	{
+		symbol = (struct symbol_t *) list_node->ptr;
+		if(!NODE_IS_MARKED(symbol, MARK_GEN))
+		{
+			grammar_symbol_print(symbol);
+		}
+		list_node = list_node->next;
+	}
+
+
 	return 0;
 }
--- p1/src/grammar_reduce.h
+++ p1/src/grammar_reduce.h
@@ -1,6 +1,8 @@
 #ifndef _GRAMMAR_REDUCE_H_
 #define _GRAMMAR_REDUCE_H_
 
+#include "grammar.h"
+
 int grammar_reduce_no_generators(struct grammar_t *g);
 
 #endif /* _GRAMMAR_REDUCE_H_ */
